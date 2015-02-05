7e204f631accfc3c622bbec4eea79003cf1076a2
yuki3738 <y.minamiya@gmail.com>
2015-02-05 20:35:55 +0900

    Add execute permission to dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
+ln -sf ~/dotfiles/.zsh.d ~/.zsh.d
