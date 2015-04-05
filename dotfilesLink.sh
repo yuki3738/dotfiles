fad63eb55d192c344cddd77a5dd00312f127da93
yuki3738 <y.minamiya@gmail.com>
2015-04-05 22:25:33 +0900

    Add .gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 ln -sf ~/dotfiles/dot.zsh.d ~/dot.zsh.d
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
