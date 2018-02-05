1e8629e49667edc3ab65ec696924cc4683c2c51f
yuki3738 <y.minamiya@gmail.com>
2018-02-05 13:36:04 +0900

    Change history.zsh

--- dot.zsh.d/history.zsh
+++ dot.zsh.d/history.zsh
@@ -1,6 +1,12 @@
-setopt hist_save_no_dups
-setopt hist_ignore_dups
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+
+setopt extended_history
 setopt hist_ignore_all_dups
+setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_no_store
+setopt hist_reduce_blanks
+setopt hist_save_no_dups
 setopt share_history
