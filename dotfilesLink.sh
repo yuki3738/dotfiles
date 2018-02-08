c3eef6458ffdae569711c7bd5c553ce747d0032f
yuki3738 <y.minamiya@gmail.com>
2018-02-08 11:35:48 +0900

    Add .zshenv

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,6 @@
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 ln -sf ~/dotfiles/.zsh.d ~/.zsh.d
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
