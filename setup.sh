7f0090f5ee753c962ee6935575a54fb1128b44b6
yuki3738 <y.minamiya@gmail.com>
2022-04-21 11:38:03 +0900

    setup.sh 修正

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 ln -sf ${HOME}/src/github.com/yuki3738/dotfiles/.zsh.d ~/.zsh.d
 
- DOT_FILES=(Brewfile .gitconfig .gitignore .gitignore_global .tmux.conf .vimrc .zshrc .ideavimrc .gitmessage)
+DOT_FILES=(Brewfile .gitconfig .gitignore .gitignore_global .tmux.conf .vimrc .zshrc .ideavimrc .gitmessage)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s ${HOME}/src/github.com/yuki3738/dotfiles/$file $HOME/$file
+    ln -sf ${HOME}/src/github.com/yuki3738/dotfiles/$file ${HOME}/$file
 done
